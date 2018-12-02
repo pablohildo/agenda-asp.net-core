cb221ef6c5916b286367241797c9058ebdc1ebb5
Pablo Hildo <pablohildo@gmail.com>
2018-12-02 19:26:01 -0300

    Add project files.

---
+++ Agenda2/Models/UsuarioEvento.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Agenda2.Models
+{
+    public partial class UsuarioEvento
+    {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public int EventoId { get; set; }
+    }
+}
---
+++ Agenda2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Agenda2.Models;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Agenda2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<agendaContext>();
+                    context.Database.EnsureCreated();
+                } catch(Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Um erro ocorreu na criação do banco de dados");
+                }
+            }
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
